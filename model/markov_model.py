f836d696ad63ae79296a3a4187eb1317878ba6b1
Ian Horsman <ianphorsman@gmail.com>
2018-06-27 19:12:57 -0400

    setup for pip in future

---
+++ model/markov_model.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+class MarkovModel(object):
+
+    def __init__(self):
+        pass
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='markov-model',
+    description='Implementation of markov model for NLP class',
+    url='https://github.com/Ianphorsman/MarkovModel',
+    author='Ian Horsman',
+    author_email='ianphorsman@gmail.com',
+    license='MIT',
+    version='0.0.1',
+    packages=['model']
+)
